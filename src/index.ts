c8c27b3e0268786b6ead9e720e8cd42f66336e63
Louis Ngatale <louisngatale@gmail.com>
2022-04-09 17:43:02 +0300

    feature: sms api

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 export * from './OTP';
+export * from './SMS';
 
+// TODO: Fix error in tests when the following is removed:
 export const sum = (a: number, b: number) => {
   if ('development' === process.env.NODE_ENV) {
     console.log('boop');
